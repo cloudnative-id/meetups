59cd3aeddca43ecfc9546b3dc697d0f6d53cfac9
Irvi Firqotul Aini <irvi.fa@gmail.com>
2019-08-31 23:37:40 +0700

    Using default push settings.

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,6 @@ echo $GITHUB_AUTH_SECRET > ~/.git-credentials && chmod 0600 ~/.git-credentials
 git config --global credential.helper store
 git config --global user.email "cncfidbot@users.noreply.github.com"
 git config --global user.name "CNCF ID Bot"
-git config --global push.default simple
 
 git add .
 git commit -m "Updating meetups list on `date`, commit ${TRAVIS_COMMIT} and job ${TRAVIS_JOB_NUMBER}" || true
