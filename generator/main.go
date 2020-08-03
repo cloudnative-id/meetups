48a59927543c6687812585800ddb097c00d5f993
Giri Kuncoro <giri.kuncoro@go-jek.com>
2020-08-03 09:26:58 +0700

    Revert "Update generator to latest format"
    This reverts commit a877ae5ce840d6c62bf7bd4b1aa854d07a186c2f.

--- generator/main.go
+++ generator/main.go
@@ -1,32 +1,23 @@
 package main
 
+// Code is heavily inspired from https://github.com/cloud-native-nordics/meetups/generator
+
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"sync"
 	"text/template"
 
-	log "github.com/sirupsen/logrus"
 	"github.com/spf13/pflag"
 	"sigs.k8s.io/yaml"
 )
 
-var speakersFile = pflag.String("speakers-file", "speakers.yaml", "Point to the speakers.yaml file")
-var companiesFile = pflag.String("companies-file", "companies.yaml", "Point to the companies.yaml file")
-var rootDir = pflag.String("meetups-dir", ".", "Point to the directory that has all meetup groups as subfolders, each with a meetup.yaml file")
-var dryRun = pflag.Bool("dry-run", true, "Whether to actually apply the changes or not")
-var validateFlag = pflag.Bool("validate", false, "Whether to validate the current state of the repo content with the spec")
+var speakersFile = pflag.String("speakers-file", "speakers.yaml", "File that contains people who have spoken at meetup groups")
+var companiesFile = pflag.String("companies-file", "companies.yaml", "File that contains companies who have sponsored and spoken at meetup groups")
+var rootDir = pflag.String("meetups-dir", ".", "Directory that has all meetup groups as subfolders, each subfolder contains a meetup.yaml file")
 var unmarshal = yaml.UnmarshalStrict
-
-// this maps the locations returned from meetup.com to what we want to use here.
-// TODO: Maybe skip this and just use "Århus" directly in our
-var cityNameExceptions = map[string]string{
-	"Århus": "Aarhus",
-}
 
 func main() {
 	if err := run(); err != nil {
@@ -37,40 +28,38 @@ func main() {
 
 func run() error {
 	pflag.Parse()
-	cfg, err := load(*companiesFile, *speakersFile, *rootDir)
+	cfg, err := load(*speakersFile, *companiesFile, *rootDir)
 	if err != nil {
 		return err
 	}
-	if err := update(cfg); err != nil {
-		return err
-	}
+
 	out, err := exec(cfg)
 	if err != nil {
 		return err
 	}
-	if *validateFlag {
-		return validate(out, *rootDir)
-	}
 	return apply(out, *rootDir)
 }
 
-func load(companiesPath, speakersPath, meetupsDir string) (*Config, error) {
+func load(speakersFile, companiesFile, meetupsDir string) (*Config, error) {
+
 	companies := []Company{}
-	companiesContent, err := ioutil.ReadFile(companiesPath)
+	companiesData, err := ioutil.ReadFile(companiesFile)
 	if err != nil {
 		return nil, err
 	}
-	if err := unmarshal(companiesContent, &companies); err != nil {
+	if err := unmarshal(companiesData, &companies); err != nil {
 		return nil, err
 	}
+
 	speakers := []Speaker{}
-	speakersContent, err := ioutil.ReadFile(speakersPath)
+	speakersData, err := ioutil.ReadFile(speakersFile)
 	if err != nil {
 		return nil, err
 	}
-	if err := unmarshal(speakersContent, &speakers); err != nil {
+	if err := unmarshal(speakersData, &speakers); err != nil {
 		return nil, err
 	}
+
 	meetupGroups := []MeetupGroup{}
 
 	err = filepath.Walk(meetupsDir, func(path string, info os.FileInfo, err error) error {
@@ -80,50 +69,65 @@ func load(companiesPath, speakersPath, meetupsDir string) (*Config, error) {
 		if !info.IsDir() {
 			return nil
 		}
-		// Consider only subdirectories of the root path
-		if filepath.Dir(path) != "." {
-			return nil
-		}
+
 		meetupsFile := filepath.Join(path, "meetup.yaml")
 		if _, err := os.Stat(meetupsFile); os.IsNotExist(err) {
 			return nil
 		} else if err != nil {
 			return err
 		}
+
 		mg := MeetupGroup{}
-		mgContent, err := ioutil.ReadFile(meetupsFile)
+		mg.FilePath = path
+		mgData, err := ioutil.ReadFile(meetupsFile)
 		if err != nil {
 			return err
 		}
-		if err := unmarshal(mgContent, &mg); err != nil {
+		if err := unmarshal(mgData, &mg); err != nil {
 			return err
 		}
 		meetupGroups = append(meetupGroups, mg)
+
 		return nil
 	})
+
 	if err != nil {
 		return nil, err
 	}
-	var wg sync.WaitGroup
-	wg.Add(len(meetupGroups))
-	// Run the fetching from the meetup API in parallel for all meetup groups to speed things up
-	for i := range meetupGroups {
-		go func(mg *MeetupGroup) {
-			defer wg.Done()
-			mg.AutogenMeetupGroup, err = GetMeetupInfoFromAPI(*mg)
-			if err != nil {
-				log.Fatal(err)
-			}
-			mg.ApplyGeneratedData()
-		}(&meetupGroups[i])
-	}
-	wg.Wait()
 
 	return &Config{
 		Speakers:     speakers,
 		Companies:    companies,
 		MeetupGroups: meetupGroups,
 	}, nil
+}
+
+func exec(cfg *Config) (map[string][]byte, error) {
+	result := map[string][]byte{}
+	for _, mg := range cfg.MeetupGroups {
+		err := mg.SetMeetupList()
+		if err != nil {
+			return nil, err
+		}
+
+		b, err := tmpl(readmeTmpl, mg)
+		if err != nil {
+			return nil, err
+		}
+
+		path := filepath.Join(mg.FilePath, "README.md")
+		result[path] = b
+	}
+
+	return result, nil
+}
+
+func tmpl(t *template.Template, obj interface{}) ([]byte, error) {
+	var buf bytes.Buffer
+	if err := t.Execute(&buf, obj); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
 }
 
 func apply(files map[string][]byte, rootDir string) error {
@@ -136,138 +140,7 @@ func apply(files map[string][]byte, rootDir string) error {
 	return nil
 }
 
-func validate(files map[string][]byte, rootDir string) error {
-	for path, fileContent := range files {
-		fullPath := filepath.Join(rootDir, path)
-		actual, err := ioutil.ReadFile(fullPath)
-		if err != nil {
-			return err
-		}
-		if !bytes.Equal(actual, fileContent) {
-			return fmt.Errorf("%s differs from expected state. expected: \"%s\", actual: \"%s\"", fullPath, fileContent, actual)
-		}
-	}
-	log.Info("Validation succeeded!")
-	return nil
-}
-
-func tmpl(t *template.Template, obj interface{}) ([]byte, error) {
-	var buf bytes.Buffer
-	if err := t.Execute(&buf, obj); err != nil {
-		return nil, err
-	}
-	return buf.Bytes(), nil
-}
-
-func exec(cfg *Config) (map[string][]byte, error) {
-	result := map[string][]byte{}
-	shouldMarshalAutoMeetup = false
-	for _, mg := range cfg.MeetupGroups {
-		mg.SetMeetupList()
-		b, err := tmpl(readmeTmpl, mg)
-		if err != nil {
-			return nil, err
-		}
-		city := mg.CityLowercase()
-		path := filepath.Join(city, "README.md")
-		result[path] = b
-
-		path = filepath.Join(city, "meetup.yaml")
-		mg.AutogenMeetupGroup = nil
-		meetupYAML, err := yaml.Marshal(mg)
-		if err != nil {
-			return nil, err
-		}
-		result[path] = meetupYAML
-	}
-	companiesYAML, err := yaml.Marshal(cfg.Companies)
-	if err != nil {
-		return nil, err
-	}
-	result["companies.yaml"] = companiesYAML
-	speakersYAML, err := yaml.Marshal(cfg.Speakers)
-	if err != nil {
-		return nil, err
-	}
-	result["speakers.yaml"] = speakersYAML
-	readmeBytes, err := tmpl(toplevelTmpl, cfg)
-	if err != nil {
-		return nil, err
-	}
-	result["README.md"] = readmeBytes
-	shouldMarshalAutoMeetup = true
-	configJSON, err := json.MarshalIndent(cfg, "", "  ")
-	if err != nil {
-		return nil, err
-	}
-	result["config.json"] = configJSON
-	stats, err := aggregateStats(cfg)
-	if err != nil {
-		return nil, err
-	}
-	statsJSON, err := json.MarshalIndent(stats, "", "  ")
-	if err != nil {
-		return nil, err
-	}
-	result["stats.json"] = statsJSON
-	return result, nil
-}
-
-func update(cfg *Config) error {
-	for i := range cfg.MeetupGroups {
-		mg := &cfg.MeetupGroups[i]
-
-		calcSponsorTiers(mg)
-
-		for j, m := range mg.Meetups {
-			if err := setPresentationTimestamps(&m); err != nil {
-				return err
-			}
-			mg.Meetups[j] = m
-		}
-	}
-	return nil
-}
-
-func calcSponsorTiers(mg *MeetupGroup) {
-	mg.SponsorTiers = map[CompanyID]SponsorTier{}
-	for _, c := range mg.EcosystemMembers {
-		if c.Company != nil {
-			mg.SponsorTiers[c.ID] = SponsorTierEcosystemMember
-		}
-	}
-	for _, m := range mg.Meetups {
-		for _, p := range m.Presentations {
-			for _, s := range p.Speakers {
-				if s.Company.Company != nil {
-					mg.SponsorTiers[s.Company.ID] = SponsorTierSpeakerProvider
-				}
-			}
-		}
-	}
-	for _, o := range mg.Organizers {
-		if o.Company.Company != nil {
-			mg.SponsorTiers[o.Company.ID] = SponsorTierMeetup
-		}
-	}
-	for _, m := range mg.Meetups {
-		for _, s := range m.Sponsors {
-			if s.Company.Company != nil {
-				if s.Role == SponsorRoleLongterm {
-					mg.SponsorTiers[s.Company.ID] = SponsorTierLongterm
-				} else {
-					mg.SponsorTiers[s.Company.ID] = SponsorTierMeetup
-				}
-			}
-		}
-	}
-}
-
 func writeFile(path string, b []byte) error {
-	if *dryRun {
-		fmt.Printf("Would write file %q with contents \"%s\"\n", path, string(b))
-		return nil
-	}
 	if err := os.MkdirAll(filepath.Dir(path), 0755); err != nil {
 		return err
 	}
--- generator/types.go
+++ generator/types.go
@@ -4,92 +4,110 @@ import (
 	"encoding/json"
 	"fmt"
 	"sort"
-	"strings"
 	"time"
 
-	log "github.com/sirupsen/logrus"
+	"github.com/rs/zerolog/log"
 )
+
+type SpeakerID string
+type CompanyID string
+type MeetupGroupID string
 
 var (
-	globalSpeakerMap        = map[SpeakerID]*Speaker{}
-	globalCompanyMap        = map[CompanyID]*Company{}
-	shouldMarshalAutoMeetup = false
+	globalCompanyMap = map[CompanyID]*Company{}
+	globalSpeakerMap = map[SpeakerID]*Speaker{}
 )
-
-type CompanyID string
-type SpeakerID string
-
-type StatsFile struct {
-	MeetupGroups uint64                 `json:"meetupGroups"`
-	AllMeetups   MeetupStats            `json:"allMeetups"`
-	PerMeetup    map[string]MeetupStats `json:"perMeetup"`
-}
-
-type MeetupStats struct {
-	Sponsors      uint64                 `json:"sponsors"`
-	SponsorByTier map[SponsorTier]uint64 `json:"sponsorByTier,omitempty"`
-	Speakers      uint64                 `json:"speakers"`
-	Meetups       uint64                 `json:"meetups"`
-	Members       uint64                 `json:"members"`
-	TotalRSVPs    uint64                 `json:"totalRSVPs"`
-	AverageRSVPs  uint64                 `json:"averageRSVPs"`
-	UniqueRSVPs   uint64                 `json:"uniqueRSVPs"`
-}
-
-type Config struct {
-	Companies    []Company     `json:"companies"`
-	Speakers     []Speaker     `json:"speakers"`
-	MeetupGroups []MeetupGroup `json:"meetupGroups"`
-}
-
-var _ json.Marshaler = &CompanyRef{}
-var _ json.Unmarshaler = &CompanyRef{}
-var _ json.Unmarshaler = &Company{}
-var _ json.Marshaler = &SpeakerRef{}
-var _ json.Unmarshaler = &SpeakerRef{}
-var _ json.Unmarshaler = &Speaker{}
 
 type SponsorRole string
 
 var (
-	SponsorRoleVenue    SponsorRole = "Venue"
-	SponsorRoleLongterm SponsorRole = "Longterm"
-	SponsorRoleCloud    SponsorRole = "Cloud"
-	SponsorRoleFood     SponsorRole = "Food"
-	SponsorRoleOther    SponsorRole = "Other"
-
-	ValidSponsorRoles = map[SponsorRole]struct{}{
-		SponsorRoleVenue:    {},
-		SponsorRoleLongterm: {},
-		SponsorRoleCloud:    {},
-		SponsorRoleFood:     {},
-		SponsorRoleOther:    {},
-	}
+	SponsorRoleVenue SponsorRole = "Venue"
+	SponsorRoleFood  SponsorRole = "Food"
+	SponsorRoleOther SponsorRole = "Other"
 )
 
-func (c *SponsorRole) UnmarshalJSON(b []byte) error {
-	str := ""
-	if err := json.Unmarshal(b, &str); err != nil {
-		return err
+type Speaker struct {
+	speakerInternal
+}
+
+type speakerInternal struct {
+	ID       SpeakerID  `json:"id"`
+	Name     string     `json:"name"`
+	Title    string     `json:"title,omitempty"`
+	Company  CompanyRef `json:"company"`
+	Github   string     `json:"github"`
+	Twitter  string     `json:"twitter,omitempty"`
+	Linkedin string     `json:"linkedin,omitempty"`
+}
+
+func (s *Speaker) UnmarshalJSON(b []byte) error {
+	stest := speakerInternal{}
+	if err := json.Unmarshal(b, &stest); err != nil {
+		return fmt.Errorf("couldn't marshal speaker %q: %v", string(b), err)
 	}
-	if _, ok := ValidSponsorRoles[SponsorRole(str)]; !ok {
-		return fmt.Errorf("not a valid sponsor role: %q", str)
+	s.speakerInternal = stest
+	if _, ok := globalSpeakerMap[s.ID]; ok {
+		log.Fatal().Msgf("Duplicate speaker found: %q", s.ID)
 	}
-	*c = SponsorRole(str)
+	if s.Company.Company == nil {
+		log.Warn().Msgf("Speaker %q doesn't have a company", s.ID)
+	}
+	globalSpeakerMap[s.ID] = s
 	return nil
 }
 
-type SponsorTier string
+func (s Speaker) String() string {
+	str := s.Name
+	if len(s.Github) != 0 {
+		str += fmt.Sprintf(" [@%s](https://github.com/%s)", s.Github, s.Github)
+	}
+	if len(s.Title) != 0 {
+		str += fmt.Sprintf(", %s", s.Title)
+	}
+	if s.Company.Company != nil {
+		str += fmt.Sprintf(", [%s](%s)", s.Company.Name, s.Company.WebsiteURL)
+	}
+	return str
+}
 
-var (
-	SponsorTierLongterm        SponsorTier = "Longterm"
-	SponsorTierMeetup          SponsorTier = "Meetup"
-	SponsorTierSpeakerProvider SponsorTier = "SpeakerProvider"
-	SponsorTierEcosystemMember SponsorTier = "EcosystemMember"
-)
+type SpeakerRef struct {
+	*Speaker `json:"-"`
+}
+
+func (s SpeakerRef) MarshalJSON() ([]byte, error) {
+	if s.Speaker == nil {
+		return []byte(`""`), nil
+	}
+	return []byte(`"` + s.ID + `"`), nil
+}
+
+func (s *SpeakerRef) UnmarshalJSON(b []byte) error {
+	if string(b) == "null" || string(b) == `""` {
+		*s = SpeakerRef{}
+		return nil
+	}
+	sid := SpeakerID("")
+	if err := json.Unmarshal(b, &sid); err != nil {
+		return fmt.Errorf("couldn't marshal speaker %q: %v", string(b), err)
+	}
+	speaker, ok := globalSpeakerMap[sid]
+	if !ok {
+		log.Fatal().Msgf("Speaker reference not found %q: %q", sid, string(b))
+	}
+	*s = SpeakerRef{speaker}
+	return nil
+}
 
 type Company struct {
 	companyInternal
+}
+
+func (c Company) String() string {
+	var str string
+	if len(c.Name) != 0 {
+		str += fmt.Sprintf(" [%s](%s)", c.Name, c.WebsiteURL)
+	}
+	return str
 }
 
 type companyInternal struct {
@@ -97,7 +115,6 @@ type companyInternal struct {
 	Name       string    `json:"name"`
 	WebsiteURL string    `json:"websiteURL"`
 	LogoURL    string    `json:"logoURL"`
-	WhiteLogo  bool      `json:"whiteLogo,omitempty"`
 }
 
 func (c *Company) UnmarshalJSON(b []byte) error {
@@ -107,7 +124,7 @@ func (c *Company) UnmarshalJSON(b []byte) error {
 	}
 	c.companyInternal = ctest
 	if _, ok := globalCompanyMap[c.ID]; ok {
-		log.Fatalf("Duplicate company found: %q", c.ID)
+		log.Fatal().Msgf("Duplicate company found: %q", c.ID)
 	}
 	globalCompanyMap[c.ID] = c
 	return nil
@@ -136,147 +153,34 @@ func (c *CompanyRef) UnmarshalJSON(b []byte) error {
 
 	company, ok := globalCompanyMap[cid]
 	if !ok {
-		log.Fatalf("Company reference not found %q: %q", cid, string(b))
+		log.Fatal().Msgf("Company reference not found %q: %q", cid, string(b))
 	}
 	*c = CompanyRef{company}
 	return nil
 }
 
-type Speaker struct {
-	speakerInternal
-}
-
-type speakerInternal struct {
-	ID             SpeakerID  `json:"id"`
-	Name           string     `json:"name"`
-	Title          string     `json:"title,omitempty"`
-	Email          string     `json:"email"`
-	Company        CompanyRef `json:"company"`
-	Github         string     `json:"github"`
-	Twitter        string     `json:"twitter,omitempty"`
-	SpeakersBureau string     `json:"speakersBureau"`
-}
-
-func (s *Speaker) UnmarshalJSON(b []byte) error {
-	stest := speakerInternal{}
-	if err := json.Unmarshal(b, &stest); err != nil {
-		return fmt.Errorf("couldn't marshal speaker %q: %v", string(b), err)
-	}
-	s.speakerInternal = stest
-	if _, ok := globalSpeakerMap[s.ID]; ok {
-		log.Fatalf("Duplicate speaker found: %q", s.ID)
-	}
-	if s.Company.Company == nil {
-		log.Warnf("Speaker %q doesn't have a company", s.ID)
-	}
-	globalSpeakerMap[s.ID] = s
-	return nil
-}
-
-func (s Speaker) String() string {
-	str := s.Name
-	if len(s.Github) != 0 {
-		str += fmt.Sprintf(" [@%s](https://github.com/%s)", s.Github, s.Github)
-	}
-	if len(s.Title) != 0 {
-		str += fmt.Sprintf(", %s", s.Title)
-	}
-	if s.Company.Company != nil {
-		str += fmt.Sprintf(", [%s](%s)", s.Company.Name, s.Company.WebsiteURL)
-	}
-	if len(s.SpeakersBureau) != 0 {
-		str += fmt.Sprintf(", [Contact](https://www.cncf.io/speaker/%s)", s.SpeakersBureau)
-	}
-	return str
-}
-
-type SpeakerRef struct {
-	*Speaker `json:"-"`
-}
-
-func (s SpeakerRef) MarshalJSON() ([]byte, error) {
-	if s.Speaker == nil {
-		return []byte(`""`), nil
-	}
-	return []byte(`"` + s.ID + `"`), nil
-}
-
-func (s *SpeakerRef) UnmarshalJSON(b []byte) error {
-	if string(b) == "null" || string(b) == `""` {
-		*s = SpeakerRef{}
-		return nil
-	}
-	sid := SpeakerID("")
-	if err := json.Unmarshal(b, &sid); err != nil {
-		return fmt.Errorf("couldn't marshal speaker %q: %v", string(b), err)
-	}
-	speaker, ok := globalSpeakerMap[sid]
-	if !ok {
-		log.Fatalf("Speaker reference not found %q: %q", sid, string(b))
-	}
-	*s = SpeakerRef{speaker}
-	return nil
-}
-
-type AutogenMeetupGroup struct {
-	Photo        string                    `json:"photo,omitempty"`
-	Name         string                    `json:"name"`
-	City         string                    `json:"city"`
-	Country      string                    `json:"country"`
-	Description  string                    `json:"description"`
-	SponsorTiers map[CompanyID]SponsorTier `json:"sponsorTiers"`
-	AutoMeetups  map[string]AutogenMeetup  `json:"-"`
-
-	members uint64
-}
-
 type MeetupGroup struct {
-	*AutogenMeetupGroup `json:",inline,omitempty"`
-
-	MeetupID          string            `json:"meetupID"`
-	Organizers        []SpeakerRef      `json:"organizers"`
-	IgnoreMeetupDates []string          `json:"ignoreMeetupDates,omitempty"`
-	CFP               string            `json:"cfpLink"`
-	Latitude          float64           `json:"latitude"`
-	Longitude         float64           `json:"longitude"`
-	EcosystemMembers  []CompanyRef      `json:"ecosystemMembers"`
-	Meetups           map[string]Meetup `json:"meetups"`
-	MeetupList        MeetupList        `json:"-"`
+	FilePath   string
+	MeetupID   string            `json:"meetupID"`
+	MeetupName string            `json:"meetupName"`
+	Organizers []string          `json:"organizers"`
+	Meetups    map[string]Meetup `json:"meetups"`
+	MeetupList MeetupList        `json:"-"`
 }
 
-func (mg *MeetupGroup) ApplyGeneratedData() {
-	for key := range mg.AutoMeetups {
-		m, ok := mg.Meetups[key]
-		if !ok {
-			found := false
-			for _, date := range mg.IgnoreMeetupDates {
-				if key == date {
-					found = true
-				}
-			}
-			if !found {
-				log.Warnf("Didn't find information about meetup at %s on date %q\n", mg.Name, key)
-			}
-			continue
+func (mg *MeetupGroup) SetMeetupList() error {
+	mlist := []Meetup{}
+	for d, m := range mg.Meetups {
+		t, err := time.Parse("20060102", d)
+		if err != nil {
+			return err
 		}
-		autoMeetup := mg.AutoMeetups[key]
-		m.AutogenMeetup = &autoMeetup
-		mg.Meetups[key] = m
+		m.Date = Time{t}
+		mlist = append(mlist, m)
 	}
-}
-
-// CityLowercase gets the lowercase variant of the city
-func (mg *MeetupGroup) CityLowercase() string {
-	return strings.ToLower(mg.City)
-}
-
-func (mg *MeetupGroup) SetMeetupList() {
-	marr := []Meetup{}
-	for _, m := range mg.Meetups {
-		marr = append(marr, m)
-	}
-	mg.MeetupList = MeetupList(marr)
+	mg.MeetupList = MeetupList(mlist)
 	sort.Sort(mg.MeetupList)
+	return nil
 }
 
 // MeetupList is a slice of meetups implementing sort.Interface
@@ -296,43 +200,19 @@ func (ml MeetupList) Swap(i, j int) {
 	ml[i], ml[j] = ml[j], ml[i]
 }
 
-type AutogenMeetup struct {
-	ID        uint64   `json:"id"`
-	Photo     string   `json:"photo,omitempty"`
-	Name      string   `json:"name"`
-	Date      Time     `json:"date,omitempty"`
-	Duration  Duration `json:"duration,omitempty"`
-	Attendees uint64   `json:"attendees,omitempty"`
-	Address   string   `json:"address"`
-
-	// rsvps map the user ID to how many rsvp's they used at this event (themselves + guests)
-	rsvps map[uint64]uint64
-}
-
-type HumanMeetup struct {
+type Meetup struct {
+	Date          Time            `json:"date,omitempty"`
+	Title         string          `json:"title"`
 	Recording     string          `json:"recording"`
 	Sponsors      []MeetupSponsor `json:"sponsors"`
 	Presentations []Presentation  `json:"presentations"`
 }
 
-type Meetup struct {
-	*AutogenMeetup `json:",inline,omitempty"`
-	HumanMeetup    `json:",inline"`
-}
-
-type fullMeetup struct {
-	*AutogenMeetup `json:",inline,omitempty"`
-	HumanMeetup    `json:",inline"`
-}
-
-func (m Meetup) MarshalJSON() ([]byte, error) {
-	if shouldMarshalAutoMeetup {
-		return json.Marshal(fullMeetup{
-			AutogenMeetup: m.AutogenMeetup,
-			HumanMeetup:   m.HumanMeetup,
-		})
-	}
-	return json.Marshal(m.HumanMeetup)
+func (m *Meetup) DateTime() string {
+	d := m.Date.UTC()
+	year, month, day := d.Date()
+	weekday := d.Weekday()
+	return fmt.Sprintf("%s, %d %s %d", weekday, day, month, year)
 }
 
 type MeetupSponsor struct {
@@ -340,30 +220,16 @@ type MeetupSponsor struct {
 	Company CompanyRef  `json:"company"`
 }
 
-func (m *Meetup) DateTime() string {
-	d := m.Date.UTC()
-	year, month, day := d.Date()
-	hour, min, _ := d.Clock()
-	hour2, min2, _ := d.Add(m.Duration.Duration).Clock()
-	return fmt.Sprintf("%d %s, %d at %d:%02d - %d:%02d", day, month, year, hour, min, hour2, min2)
-}
-
 type Presentation struct {
-	Duration  Duration     `json:"duration"`
-	Delay     *Duration    `json:"delay,omitempty"`
+	Duration  string       `json:"duration"`
 	Title     string       `json:"title"`
 	Slides    string       `json:"slides"`
 	Recording string       `json:"recording,omitempty"`
 	Speakers  []SpeakerRef `json:"speakers"`
-
-	start time.Time
-	end   time.Time
 }
 
-func (p *Presentation) StartTime() string {
-	return fmt.Sprintf("%d:%02d", p.start.UTC().Hour(), p.start.UTC().Minute())
-}
-
-func (p *Presentation) EndTime() string {
-	return fmt.Sprintf("%d:%02d", p.end.UTC().Hour(), p.end.UTC().Minute())
+type Config struct {
+	Companies    []Company     `json:"companies"`
+	Speakers     []Speaker     `json:"speakers"`
+	MeetupGroups []MeetupGroup `json:"meetupGroups"`
 }
